f083e9d4bc7a6882a7897d18e92baea6fc261cf6
Denis Bazhenov <dotsid@gmail.com>
2022-02-09 22:04:13 +1000

    Modification time is added to the output

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::{
     ffi::OsStr,
     fs::{self, File},
     path::PathBuf,
+    time::Instant,
 };
 
 use anyhow::Result;
+use chrono::{DateTime, TimeZone, Utc};
 use rusqlite::Connection;
 use thiserror::Error;
 
@@ -22,6 +24,7 @@ enum Errors {
 struct Annotation {
     selected_text: Option<String>,
     note: Option<String>,
+    creation_date: DateTime<Utc>,
 }
 
 fn main() -> Result<()> {
@@ -29,21 +32,29 @@ fn main() -> Result<()> {
     println!("{:?}", db_path);
     let connection = Connection::open(db_path)?;
 
-    let mut stms =
-        connection.prepare("select ZANNOTATIONSELECTEDTEXT, ZANNOTATIONNOTE from ZAEANNOTATION")?;
+    let mut stms = connection.prepare(
+        "select
+            ZANNOTATIONSELECTEDTEXT,
+            ZANNOTATIONNOTE,
+            ZANNOTATIONMODIFICATIONDATE
+         from ZAEANNOTATION
+         where ZANNOTATIONSELECTEDTEXT IS NOT NULL
+         ORDER BY ZANNOTATIONMODIFICATIONDATE",
+    )?;
     let annotations = stms.query_map([], |row| {
+        let ts: f32 = row.get(2)?;
         Ok(Annotation {
             selected_text: row.get(0)?,
             note: row.get(1)?,
+            creation_date: core_data_to_timestamp(ts as i64),
         })
     })?;
 
     for a in annotations {
         let a = a?;
-        println!(
-            "Text: {}",
-            a.selected_text.as_ref().map(String::as_str).unwrap_or("-")
-        );
+        println!("Date: {}", a.creation_date);
+        let text = a.selected_text.as_ref().map(String::as_str).unwrap_or("-");
+        println!("Text: {}", text);
         if let Some(note) = a.note {
             println!("Note: {}", note);
         }
@@ -68,4 +79,8 @@ fn locate_annotation_database() -> Result<Option<PathBuf>> {
     }
 
     Ok(None)
+}
+
+fn core_data_to_timestamp(ts: i64) -> DateTime<Utc> {
+    Utc.timestamp(ts + 978307200, 0)
 }
